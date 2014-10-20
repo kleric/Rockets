0d09a6cd424c18eff08e60ef450954c4b78208c7
Clark Chen <clark@cmu.edu>
2014-10-20 00:45:47 -0400

    Initial Commit

---
+++ gen/com/happycacti/rockets/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.happycacti.rockets;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/happycacti/rockets/MenuActivity.java
@@ -0,0 +1,7 @@
+package com.happycacti.rockets;
+
+import android.app.Activity;
+
+public class MenuActivity extends Activity {
+
+}
