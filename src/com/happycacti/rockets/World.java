2caf2f2c62c461f0412c4ab45869b03d6b482c02
Clark Chen <clark@kleric.org>
2014-10-23 03:06:54 -0400

    Synchronized Rockets
    Now we won't crash because of thread issues

--- src/com/happycacti/rockets/World.java
+++ src/com/happycacti/rockets/World.java
@@ -7,15 +7,15 @@ import android.graphics.Canvas;
 public class World {
 	public static final float GRAVITY_ACCELERATION = 0.001f;
 	public static final float PLAYER_TERMINAL_VELOCITY = 0.8f;
-	
+
 	private Player player;
 	private Camera camera;
-	
+
 	private int width, height;
-	
+
 	private ArrayList<Platform> platforms;
 	private ArrayList<Rocket> rockets;
-	
+
 	public void press(float x, float y) {
 		player.press(this, camera.getRealX(x), camera.getRealY(y));
 	}
@@ -36,24 +36,30 @@ public class World {
 	public void draw(Canvas canvas) {
 		canvas.drawRGB(127, 127, 127);
 		player.draw(canvas, camera);
-		
-		for(Rocket r : rockets) {
-			r.draw(canvas, camera);
+
+		synchronized(rockets) {
+			for(Rocket r : rockets) {
+				r.draw(canvas, camera);
+			}
 		}
 	}
-	
+
 	public void update(int delta) {
 		//Update entities based on time change
 		player.update(delta);
-		
-		for(Rocket r : rockets) {
-			r.update(delta);
+
+		synchronized(rockets) {
+			for(Rocket r : rockets) {
+				r.update(delta);
+			}
 		}
-		
+
 		//Point the camera at the player (sorta)
 		camera.update(player);
 	}
 	public void createRocket(Rocket rocket) {
-		rockets.add(rocket);
+		synchronized(rockets) {
+			rockets.add(rocket);
+		}
 	}
 }
